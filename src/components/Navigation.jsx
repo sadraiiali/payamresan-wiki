3febdc48ad98c8002b575f3d8df9ad65f15e5a9e
Jade9525 <167679995+Jade9525@users.noreply.github.com>
2024-04-28 13:17:46 +0330

    add standard chater

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -231,6 +231,7 @@ export const navigation = [
     links: [
       { title: 'پیامرسان آزاد؟', href: '/why-open-messenger' },
       { title: 'چرا؟!', href: '/why' },
+      {title: 'استانداردها', href: '/standard'},
       { title: 'پیامرسان‌ها', href: '/messengers' },
       { title: 'وضعیت فعلی', href: '/latest-situation' },
     ],
